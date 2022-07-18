ad25be66acd687938075eb2f3f48d6af37af6dfd
Elisee_Mulenga <mulengaelisee2@gamil.com>
2022-07-18 17:12:11 +0200

    Creation de la page login

---
+++ app/src/main/java/com/company/projetannuel/LoginActivity.java
@@ -0,0 +1,53 @@
+package com.company.projetannuel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        EditText edMatricule = findViewById(R.id.editMatricule);
+        EditText edPassword = findViewById(R.id.editPassword);
+
+        Button boutonLogin = findViewById(R.id.btnLogin);
+        progressBar = findViewById(R.id.progressBar);
+
+        boutonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String matricule = edMatricule.getText().toString();
+                String password = edPassword.getText().toString();
+
+                if (matricule.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Le matricule est vide",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }else if (password.isEmpty()){
+                    Toast.makeText(LoginActivity.this, "Entre un mot de passe",
+                            Toast.LENGTH_LONG).show();
+                    return;
+                }else  {
+                    login(matricule,password);
+                }
+            }
+        });
+    }
+
+    private void login(String matricule, String password){
+        progressBar.setVisibility(View.VISIBLE);
+        progressBar.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.GONE);
+    }
+}
