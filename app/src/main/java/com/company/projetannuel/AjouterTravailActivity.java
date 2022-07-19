467a0a151b2699ba064312d82cfb99241b4f9e81
Elisee_Mulenga <mulengaelisee2@gamil.com>
2022-07-19 10:22:32 +0200

    Ajout de la page ajout-travail, profile et liste-travail

---
+++ app/src/main/java/com/company/projetannuel/AjouterTravailActivity.java
@@ -0,0 +1,14 @@
+package com.company.projetannuel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AjouterTravailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter_travail);
+    }
+}
---
+++ app/src/main/java/com/company/projetannuel/ListeTravailActivity.java
@@ -0,0 +1,14 @@
+package com.company.projetannuel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListeTravailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste_travail);
+    }
+}
