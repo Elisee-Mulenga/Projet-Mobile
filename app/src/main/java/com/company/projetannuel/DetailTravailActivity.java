e303cbb694c667c344a34063bcebb2b40bcfd058
Elisee_Mulenga <mulengaelisee2@gamil.com>
2022-07-25 13:21:48 +0200

    Ajout de la navigation dans l'application

--- app/src/main/java/com/company/projetannuel/DetailTravailActivity.java
+++ app/src/main/java/com/company/projetannuel/DetailTravailActivity.java
@@ -2,7 +2,10 @@ package com.company.projetannuel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DetailTravailActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class DetailTravailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_travail);
+
+        configurerButtonDetail();
+    }
+
+    private void configurerButtonDetail(){
+        Button listeBoutton = findViewById(R.id.btn_modifTrav_doc);
+        listeBoutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent listIntent = new Intent(DetailTravailActivity.this, ModifierTravailActivity.class);
+                startActivity(listIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/company/projetannuel/ProfileActivity.java
+++ app/src/main/java/com/company/projetannuel/ProfileActivity.java
@@ -2,7 +2,10 @@ package com.company.projetannuel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        configurerButtonDetail();
+    }
+
+    private void configurerButtonDetail(){
+        Button listeBoutton = findViewById(R.id.btn_modifProf_doc);
+        listeBoutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent listIntent = new Intent(ProfileActivity.this, ModifierProfilActivity.class);
+                startActivity(listIntent);
+            }
+        });
     }
 }
